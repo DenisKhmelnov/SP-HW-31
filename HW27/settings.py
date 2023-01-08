bd362778286df284a7113237e989082ee8a9ec36
Denis Khmelnov <denishmelnov@gmail.com>
2023-01-08 21:19:09 +0500

    feat: change User model

--- HW27/settings.py
+++ HW27/settings.py
@@ -139,3 +139,4 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.User'
